7da067510c6364d76ebf17064f274c5401ed9819
Lydie <mklydie23xy@gmail.com>
2026-03-09 16:42:50 +0100

    premier commit de mon portfolio

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Github, Linkedin, Mail, Briefcase } from 'lucide-react';
 
 const Navbar = () => {
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ExternalLink } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
