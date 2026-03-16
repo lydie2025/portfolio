8995a4f1895b091acba97454c520a433a790044f
Lydie <mklydie23xy@gmail.com>
2026-03-16 12:32:06 +0100

    correction des fichiers

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import About from '../components/sections/About'
-import Projects from '../components/sections/Projects'
-import Contact from '../components/sections/Contact'
-
 function AppRoutes() {
   return (
     <Routes>
